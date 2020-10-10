d933a34973800e6dd4ee85d5d98eee24f494ca9c
solcra <carlosgranadacra@gmai.com>
2020-10-09 20:44:03 -0500

    Add una funcion como modulo

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,8 @@
+import MediaPlayer from './mediaPlayer.js'
+
 const video = document.querySelector("video")
 const button = document.querySelector("button")
 
-function MediaPlayer(config) {
-  this.media = config.el
-}
 
-MediaPlayer.prototype.play = function(){
-  this.media.play()
-}
 const player = new MediaPlayer({ el: video});
 button.onclick = () => player.play();
